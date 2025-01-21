570658e9d5a1ab4539fb7839d3f9055784e41ee0
Bilal Khan <bilal.khan16@ontariotechu.net>
2025-01-21 11:59:47 -0500

    inputreadeer

--- Scrap/Assets/Scripts/InputReader.cs
+++ Scrap/Assets/Scripts/InputReader.cs
@@ -11,6 +11,21 @@ public class InputReader : MonoBehaviour, Controls.IPlayerActions
     public event Action JumpEvent;
     public event Action DodgeEvent;
     public event Action TargetEvent;
+    public event Action DetachPartEvent;
+    public event Action ReattachPartEvent;
+    public event Action ShootRightEvent;
+    public event Action ShootLeftEvent;
+
+    public event Action DropEverythingEvent;
+    public event Action DropLeftArmEvent;
+    public event Action DropRightArmEvent;
+    public event Action DropLeftLegEvent;
+    public event Action DropRightLegEvent;
+    public event Action DropBothLegsEvent;
+
+    public event Action RecallBothArmsEvent;
+    public event Action RecallLeftArmEvent;
+    public event Action RecallRightArmEvent;
 
     Controls controls;
 
@@ -59,5 +74,82 @@ public class InputReader : MonoBehaviour, Controls.IPlayerActions
             IsBlocking = true;
         else if (context.canceled)
             IsBlocking = false;
+    }
+ public void OnDetachPart(InputAction.CallbackContext context)
+    {
+        if (!context.performed) return;
+        DetachPartEvent?.Invoke();
+    }
+
+    public void OnReattachPart(InputAction.CallbackContext context)
+    {
+        if (!context.performed) return;
+        ReattachPartEvent?.Invoke();
+    }
+
+    public void OnShootR(InputAction.CallbackContext context)
+    {
+        if (!context.performed) return;
+        ShootRightEvent?.Invoke();
+    }
+
+    public void OnShootL(InputAction.CallbackContext context)
+    {
+        if (!context.performed) return;
+        ShootLeftEvent?.Invoke();
+    }
+
+    // Missing methods for new D-pad actions
+    public void OnDropEverything(InputAction.CallbackContext context)
+    {
+        if (!context.performed) return;
+        DropEverythingEvent?.Invoke();
+    }
+
+    public void OnDropLeftArm(InputAction.CallbackContext context)
+    {
+        if (!context.performed) return;
+        DropLeftArmEvent?.Invoke();
+    }
+
+    public void OnDropRightArm(InputAction.CallbackContext context)
+    {
+        if (!context.performed) return;
+        DropRightArmEvent?.Invoke();
+    }
+
+    public void OnDropLeftLeg(InputAction.CallbackContext context)
+    {
+        if (!context.performed) return;
+        DropLeftLegEvent?.Invoke();
+    }
+
+    public void OnDropRightLeg(InputAction.CallbackContext context)
+    {
+        if (!context.performed) return;
+        DropRightLegEvent?.Invoke();
+    }
+
+    public void OnDropBothLegs(InputAction.CallbackContext context)
+    {
+        if (!context.performed) return;
+        DropBothLegsEvent?.Invoke();
+    }
+
+    public void OnRecallBothArms(InputAction.CallbackContext context)
+    {
+        if(!context.performed) return;
+        RecallBothArmsEvent?.Invoke();
+    }
+    public void OnRecallRightArm(InputAction.CallbackContext context)
+    {
+        if (!context.performed) return;
+        RecallRightArmEvent?.Invoke();
+
+    }
+    public void OnRecallLeftArm(InputAction.CallbackContext context)
+    {
+        if (!context.performed) return;
+        RecallLeftArmEvent?.Invoke();
     }
 }
