57083c6a131aae109ce2b5335e3eb27209378ee1
Bilal Khan <bilal.khan16@ontariotechu.net>
2025-03-04 14:11:04 -0500

    aim sens

---
+++ Scrap/Assets/Scripts/FreeLookInputHandler.cs
@@ -0,0 +1,31 @@
+using Cinemachine;
+using UnityEngine;
+
+public class FreeLookInputHandler : MonoBehaviour
+{
+    public CinemachineFreeLook freeLookCamera;
+    public float xAxisSensitivity = 200f; 
+    public float yAxisSensitivity = 2f;   
+    public float accelerationTime = 0.2f; 
+    public float decelerationTime = 0.2f; 
+
+    private void Start()
+    {
+        if (freeLookCamera != null)
+        {
+            freeLookCamera.m_XAxis.m_AccelTime = accelerationTime;
+            freeLookCamera.m_XAxis.m_DecelTime = decelerationTime;
+            freeLookCamera.m_YAxis.m_AccelTime = accelerationTime;
+            freeLookCamera.m_YAxis.m_DecelTime = decelerationTime;
+        }
+    }
+
+    private void Update()
+    {
+        if (freeLookCamera != null)
+        {
+            freeLookCamera.m_XAxis.m_MaxSpeed = xAxisSensitivity;
+            freeLookCamera.m_YAxis.m_MaxSpeed = yAxisSensitivity;
+        }
+    }
+}
