a7e843f7815de717652fc3ec29eab1ee97d9f322
Bilal Khan <bilal.khan16@ontariotechu.net>
2025-03-05 22:46:00 -0500

    change controls

--- Scrap/Assets/Scripts/Controls.cs
+++ Scrap/Assets/Scripts/Controls.cs
@@ -428,7 +428,7 @@ public partial class @Controls: IInputActionCollection2, IDisposable
                 {
                     ""name"": """",
                     ""id"": ""b6efa3a1-c9dd-4cf5-871f-ecd89dac53d7"",
-                    ""path"": ""<Gamepad>/leftStickPress"",
+                    ""path"": ""<Gamepad>/rightStickPress"",
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": "";Gamepad"",
