ae015711237d3762d2cc89d613beb9f32ce56487
SamFabra <samfabra5@gmail.com>
2025-03-15 10:21:39 -0400

    Improvelava level purple platform

---
+++ Scrap/Assets/Scripts/Map Mechanics/Platforms/PurplePlatform.cs
@@ -0,0 +1,170 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PurplePlatform : MonoBehaviour
+{
+    [SerializeField] Transform[] waypoints;
+    [SerializeField] float speed;
+    [SerializeField] float changeDirectionDelay;
+    
+    private int currentWaypointIndex = 0;
+    private bool isTriggered = false;
+    private bool isWaiting = false;
+    private Vector3 startPosition;
+    private const float waypointThreshold = 0.05f;
+    
+    public Attach attach;
+    public CharacterController playerController = null;
+    [SerializeField] private List<Rigidbody> bodyParts = new List<Rigidbody>();
+    private Dictionary<Rigidbody, Vector3> bodyPartOriginalPositions = new Dictionary<Rigidbody, Vector3>();
+    public bool isPlayerGrappled = false;
+
+    void Start()
+    {
+        if (waypoints.Length < 2)
+        {
+            Debug.LogError("Waypoints must have at least two points!");
+            enabled = false;
+            return;
+        }
+        startPosition = transform.position;
+        SaveBodyPartOriginalPositions();
+    }
+    
+    void FixedUpdate()
+    {
+        if (attach.isPlayerGrappled)
+        {
+            playerController = null;
+        }
+        
+        if (isTriggered)
+        {
+            Move();
+        }
+        else
+        {
+            ReturnToStart();
+        }
+        
+        MovePlayerWithPlatform();
+        MoveBodyPartsWithPlatform();
+    }
+    
+    void Move()
+    {
+        if (!isWaiting && currentWaypointIndex < waypoints.Length)
+        {
+            Vector3 targetPosition = waypoints[currentWaypointIndex].position;
+            Vector3 direction = (targetPosition - transform.position).normalized;
+            transform.position += direction * speed * Time.fixedDeltaTime;
+            
+            if (Vector3.Distance(transform.position, targetPosition) <= waypointThreshold)
+            {
+                transform.position = targetPosition;
+                isWaiting = true;
+                StartCoroutine(ChangeDelay());
+            }
+        }
+    }
+    
+    void ReturnToStart()
+    {
+        if (!isWaiting && Vector3.Distance(transform.position, startPosition) > waypointThreshold)
+        {
+            Vector3 direction = (startPosition - transform.position).normalized;
+            transform.position += direction * speed * Time.fixedDeltaTime;
+        }
+        else if (!isWaiting)
+        {
+            transform.position = startPosition;
+            currentWaypointIndex = 0;
+        }
+    }
+    
+    IEnumerator ChangeDelay()
+    {
+        yield return new WaitForSeconds(changeDirectionDelay);
+        currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Length;
+        isWaiting = false;
+    }
+    
+    public void TriggerMovement()
+    {
+        isTriggered = true;
+    }
+    
+    public void StopMovement()
+    {
+        isTriggered = false;
+    }
+    
+    void MovePlayerWithPlatform()
+    {
+        if (playerController != null)
+        {
+            playerController.Move(transform.position - waypoints[currentWaypointIndex - 1].position);
+        }
+    }
+    
+    void MoveBodyPartsWithPlatform()
+    {
+        foreach (Rigidbody rb in bodyParts)
+        {
+            if (rb != null)
+            {
+                rb.position += transform.position - waypoints[currentWaypointIndex - 1].position;
+            }
+        }
+    }
+
+    void SaveBodyPartOriginalPositions()
+    {
+        foreach (Rigidbody rb in bodyParts)
+        {
+            if (rb != null && !bodyPartOriginalPositions.ContainsKey(rb))
+            {
+                bodyPartOriginalPositions[rb] = rb.position;
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            playerController = other.GetComponent<CharacterController>();
+            isTriggered = true;
+        }
+        else if (other.CompareTag("R_Arm") || other.CompareTag("L_Arm") || other.CompareTag("R_Leg") || other.CompareTag("L_Leg") || other.CompareTag("Torso"))
+        {
+            Rigidbody rb = other.GetComponent<Rigidbody>();
+            if (rb != null && !bodyParts.Contains(rb))
+            {
+                bodyParts.Add(rb);
+                if (bodyPartOriginalPositions.ContainsKey(rb))
+                {
+                    rb.position = bodyPartOriginalPositions[rb];
+                }
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            playerController = null;
+            isTriggered = false;
+        }
+        if (other.CompareTag("R_Arm") || other.CompareTag("L_Arm") || other.CompareTag("R_Leg") || other.CompareTag("L_Leg") || other.CompareTag("Torso"))
+        {
+            Rigidbody rb = other.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                bodyParts.Remove(rb);
+            }
+        }
+    }
+}
