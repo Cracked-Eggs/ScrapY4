e0f79b2bce106e4328cf610438ea82b46e894792
shezwincarim <56273578+shezwincarim@users.noreply.github.com>
2025-04-11 05:07:38 -0400

    hallwaychanges

--- Scrap/Assets/Scripts/Magnetism/Body/RadiusChecker.cs
+++ Scrap/Assets/Scripts/Magnetism/Body/RadiusChecker.cs
@@ -161,7 +161,7 @@ public class RadiusChecker : MonoBehaviour
                 {
                     flowField.SetNewTarget(transform.position);
                     Vector3 moveDirection = flowField.GetFlowDirection(bodyPart.transform.position);
-                        //moveDirection += Vector3.up * 0.08f;
+                        //moveDirection += Vector3.up * 0.5f;
                         rb.velocity = moveDirection * forceStrength;
 
                     yield return null;
